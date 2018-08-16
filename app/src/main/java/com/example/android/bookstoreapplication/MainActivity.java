b40a96f6f053aa896d313406581293aa420a645b
James Bouvier <jbouvier129@gmail.com>
2018-08-15 22:46:26 -0400

    Main Activity Update

--- app/src/main/java/com/example/android/bookstoreapplication/MainActivity.java
+++ app/src/main/java/com/example/android/bookstoreapplication/MainActivity.java
@@ -1,13 +1,106 @@
 package com.example.android.bookstoreapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.bookstoreapplication.data.BooksContract.BooksEntry;
+import com.example.android.bookstoreapplication.data.BooksDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variable to access inventory db
+    private BooksDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setup FAB to launch inventory editor
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchEditorActivity = new Intent(MainActivity.this, InventoryEditor.class);
+                startActivity(launchEditorActivity);
+            }
+        });
+
+        mDbHelper = new BooksDbHelper(this);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showInventory();
+    }
+
+    //Helper method to populate TextView with inventory data
+    private void showInventory() {
+        //Create/open readable version of inventory.db
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        //projection parameters
+        String[] projection = {
+                BooksEntry._ID,
+                BooksEntry.COLUMN_PRODUCT_NAME,
+                BooksEntry.COLUMN_PRICE,
+                BooksEntry.COLUMN_QUANTITY,
+                BooksEntry.COLUMN_SUPPLIER_NAME,
+                BooksEntry.COLUMN_SUPPLIER_PHONE_NUMBER
+        };
+        //Select query
+        Cursor cursor = db.query(
+                BooksEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        //Create view to display cursor results
+        TextView cursorView = findViewById(R.id.store_inventory);
+
+        try {
+            cursorView.setText("Table is active and has " + cursor.getCount() + " items currently. \n\n");
+            cursorView.append(BooksEntry._ID + " - " +
+                    BooksEntry.COLUMN_PRODUCT_NAME + " - " +
+                    BooksEntry.COLUMN_PRICE + " - " +
+                    BooksEntry.COLUMN_QUANTITY + " - " +
+                    BooksEntry.COLUMN_SUPPLIER_NAME + " - " +
+                    BooksEntry.COLUMN_SUPPLIER_PHONE_NUMBER + "\n"
+
+            );
+            //figure out column indexes then iterate through them so that every result from the query (cursor) is displayed
+            int idIndex = cursor.getColumnIndex(BooksEntry._ID);
+            int productIndex = cursor.getColumnIndex(BooksEntry.COLUMN_PRODUCT_NAME);
+            int priceIndex = cursor.getColumnIndex(BooksEntry.COLUMN_PRICE);
+            int quantityIndex = cursor.getColumnIndex(BooksEntry.COLUMN_QUANTITY);
+            int supplierIndex = cursor.getColumnIndex(BooksEntry.COLUMN_SUPPLIER_NAME);
+            int supplierPhoneNumberIndex = cursor.getColumnIndex(BooksEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
+
+            while (cursor.moveToNext()) {
+                int currentId = cursor.getInt(idIndex);
+                String currentProduct = cursor.getString(productIndex);
+                int currentPrice = cursor.getInt(priceIndex);
+                int currentQuantity = cursor.getInt(quantityIndex);
+                String currentSupplier = cursor.getString(supplierIndex);
+                String currentSupplierPhoneNumber = cursor.getString(supplierPhoneNumberIndex);
+
+                cursorView.append("\n" + currentId + " - " + currentProduct + " - " + currentPrice + " - " +
+                        currentQuantity + " - " + currentSupplier + " - " + currentSupplierPhoneNumber);
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+
 }
